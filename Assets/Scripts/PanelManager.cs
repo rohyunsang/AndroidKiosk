e6ba5b2ed15e9fa02a3fcac48f1129cb03d42cc2
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-10-13 15:08:42 +0900

    ig change

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -14,74 +14,58 @@ public class PanelManager : MonoBehaviour
     public GameObject PurchasePanel;
     public GameObject ZoomImagePanel;
     public GameObject NaviPanel;
-
     public GameObject MainPanel;
-
     public void OnMainPanel(){
         MainPanel.SetActive(true);
     }
     public void OffMainPanel(){
         MainPanel.SetActive(false);
     }
-
     public void InvokeOffPurchasePanel()
     { 
         PurchasePanel.SetActive(false);
         MainPanel.SetActive(true);
         InitPanel.SetActive(true);
     }
-
     public void OffPurchasePanel()
     {  //waiting 3second
         Invoke("InvokeOffPurchasePanel", 3f);
     }
-
-
     public void OnVirtualFittingPanel()
     {
         VirtualFittingPanel.SetActive(true);
     }
-
     public void OnSettingPanel()
     {
         SettingPanel.SetActive(true);
     }
-
     public void OnLoginPanel()
     {
         LoginPanel.SetActive(true);
     }
-
     public void OnInitPanel()
     {
         InitPanel.SetActive(true);
     }
-
     public void OnPurchasePanel()
     {
         PurchasePanel.SetActive(true);
         OffPurchasePanel(); // 일단 구매기능이 없으니 구매를 건너뜀.
     }
-
     public void OnZoomImagePanel()
     {
         ZoomImagePanel.SetActive(true);
     }
-    
     public void OnNaviPanel(){
         NaviPanel.SetActive(true);
     }
     public void OffNaviPanel(){
         NaviPanel.SetActive(false);
     }
-
     public void OffZoomImagePanel()
     {
         ZoomImagePanel.SetActive(false);
     }
-
-
-    
     public void OffInitPanel()
     {
         InitPanel.SetActive(false);
